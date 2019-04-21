385f8da91d8b9085cb453af355ebb16de4b5d362
AeksandraKorovko <alextim77733@gmail.ru>
2019-04-22 01:03:09 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -8,10 +8,24 @@ $('.close-popup').click(function () {
     $('.popup-wrapper').fadeOut();
 });
 
-    $('.mobile-menu').click(function () {
-        $('.navigation ul').toggleClass('active');
+$('.mobile-menu').click(function () {
+    $('.navigation ul').toggleClass('active');
+});
+
+//Main slider initialization
+
+    var mySwiper = new Swiper('.main-slider', {
+        speed: 400,
+        autoplay: {
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: '.button-next',
+            prevEl: '.button-prev',
+        },
+        loop: true,
     });
-   
+
 });
 
 
